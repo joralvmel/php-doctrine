79559fe545d0a3c6920fee2a80f51d9d0c7fe6bf
joralvmel <joralvmel@gmail.com>
2024-12-12 09:31:44 +0100

    add DELETE users on browser

--- src/controllers.php
+++ src/controllers.php
@@ -32,10 +32,18 @@ function listUsersFunction(): void
     foreach ($users as $user) {
         $updateUrl = "/users/update/" . $user->getId();
         $viewUrl = "/users/" . $user->getUsername();
+        $deleteUrl = "/users/delete/" . $user->getId();
+
         echo '<tr>';
         echo '<td>' . $user->getId() . '</td>';
         echo '<td>' . $user->getUsername() . '</td>';
-        echo '<td><a href="' . $updateUrl . '">Update</a> | <a href="' . $viewUrl . '">Read</a></td>';
+        echo '<td>
+        <a href="' . $viewUrl . '">Read</a> | 
+        <a href="' . $updateUrl . '">Update</a> |
+        <form action="' . $deleteUrl . '" method="POST" style="display:inline;">
+            <button type="submit" onclick="return confirm(\'Are you sure you want to delete this user?\')">Delete</button>
+        </form>
+    </td>';
         echo '</tr>';
     }
     echo '</table>';
@@ -130,4 +138,20 @@ function updateUserFunction(int $id): void
         </form>
         FORM;
     }
+}
+
+function deleteUserFunction(int $id): void
+{
+    $entityManager = DoctrineConnector::getEntityManager();
+    $userRepository = $entityManager->getRepository(User::class);
+    $user = $userRepository->find($id);
+
+    if ($user === null) {
+        echo 'User not found';
+        return;
+    }
+
+    $entityManager->remove($user);
+    $entityManager->flush();
+    echo 'User deleted successfully';
 }
